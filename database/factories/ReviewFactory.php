fd13df3a7e076090414c6e7fc38fe0c8d8f9707f
tharypnc <thary.sat@gmail.com>
2018-09-08 17:19:42 +0700

    Seeding part for product and review

--- database/factories/ReviewFactory.php
+++ database/factories/ReviewFactory.php
@@ -1,9 +1,15 @@
 <?php
 
+use App\Madel\Product;
 use Faker\Generator as Faker;
 
 $factory->define(App\Madel\Review::class, function (Faker $faker) {
     return [
-        //
+        'product_id' => function() {
+            return Product::all()->random();
+        },
+        'customer' =>$faker->name,
+        'review'   =>$faker->paragraph,
+        'star' =>$faker->numberBetween(0,5)
     ];
 });
