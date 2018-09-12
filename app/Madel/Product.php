8e52f12c7bb07acb23de728364f29885bfbcc1c0
tharypnc <thary.sat@gmail.com>
2018-09-12 16:00:12 +0700

    Relationship

--- app/Madel/Product.php
+++ app/Madel/Product.php
@@ -2,9 +2,12 @@
 
 namespace App\Madel;
 
+use App\Madel\Review;
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    public function reviews (){
+        return $this->hasMany(Review::class);
+    }
 }
