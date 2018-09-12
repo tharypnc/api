47d4ea14c81738742aede7a6a235548c3a4a215f
tharypnc <thary.sat@gmail.com>
2018-09-12 17:19:46 +0700

    Transforming Product

--- app/Http/Resources/Product/ProductResource.php
+++ app/Http/Resources/Product/ProductResource.php
@@ -15,11 +15,16 @@ class ProductResource extends JsonResource
     public function toArray($request)
     {
         return [
-               'name'       =>$this->name,
-               'description'=>$this->detail,
-               'price'      =>$this->price,
-               'stock'      =>$this->stock,
-               'discount'   =>$this->discount
+               'name'       =>  $this->name,
+               'description'=>  $this->detail,
+               'price'      =>  $this->price,
+               'stock'      =>  $this->stock > 0 ? $this->stock : 'Out of Stock',
+               'discount'   =>  $this->discount,
+               'totalPrice' =>  round((1-($this->discount/100))*$this->price,2),
+               'rating'     => $this->reviews->count() > 0 ? round($this->reviews->sum('star')/$this->reviews->count(),2) :'No Rating Yet',
+               'href'       => [
+                            'reviews'=> route('reviews.index',$this->id)
+               ]
         ];
     }
 }
