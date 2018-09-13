86d83ed35e6bfaee76ec2121d9869008a2751a2b
tharypnc <thary.sat@gmail.com>
2018-09-13 11:51:13 +0700

    Product Collection Recource

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Madel\Product;
 use App\Http\Resources\Product\ProductResource;
+use App\Http\Resources\Product\ProductCollection;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
@@ -15,7 +16,7 @@ class ProductController extends Controller
      */
     public function index()
     {
-        return Product::all();
+        return ProductCollection::Collection(Product::all());
     }
 
     /**
